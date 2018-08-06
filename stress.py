685771ac23b3fa021939e39e3a9bf1f5821880c4
ozcan <ozcan@semiworld.org>
2018-08-06 20:34:07 +0300

    init

---
+++ stress.py
@@ -0,0 +1,12 @@
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+
+def f(x):
+    while True:
+        x*x
+
+if __name__ == '__main__':
+    processes = cpu_count() - 1
+    print('utilizing %d cores\n' % processes)
+    pool = Pool(processes)
+    pool.map(f, range(processes))
